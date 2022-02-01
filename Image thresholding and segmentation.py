c75d3c351e455becf1856243083291ca4de0a08a
punethahimanshu <97303222+punethahimanshu@users.noreply.github.com>
2022-02-01 21:10:50 +0530

    Add files via upload

---
+++ Image thresholding and segmentation.py
@@ -0,0 +1,11 @@
+
+"""
+Manual and auto thresholding
+@author: HIMANSHU
+
+"""
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread("Images/Osteosarcoma_01.tif", 1)
+plt.imshow(img)
